7879150d7e4e26ecabd6c7126cf7f8293c34a7eb
aleksandra-majchrzak <aleksandra.a.majchrzak@gmail.com>
2017-08-10 23:59:07 +0200

    Initial dummy SSL config.

---
+++ src/main/java/Constants.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Mohru on 10.08.2017.
+ */
+public interface Constants {
+    String KEYSTORE_PATH = "smart_keystore.jks";
+    String KEYSTORE_PASS = "Smart_password1";
+    String CERTIFICATE_PATH = "X509_certificate.cer";
+}
--- src/main/java/WebApp.java
+++ src/main/java/WebApp.java
@@ -4,6 +4,21 @@ import controllers.UserController;
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.TrustManagerFactory;
+import javax.net.ssl.X509TrustManager;
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.KeyManagementException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -15,6 +30,8 @@ import static spark.Spark.*;
 public class WebApp {
 
     public static void initialize() {
+
+        secure(Constants.KEYSTORE_PATH, Constants.KEYSTORE_PASS, null, null);
 
         // default port 4567
         staticFileLocation("/public");
@@ -75,5 +92,34 @@ public class WebApp {
 
         get("/modules/:id", ModulesController::getModule,
                 new VelocityTemplateEngine());
+    }
+
+    private TrustManager initTrustManager() {
+        try {
+            CertificateFactory factory = CertificateFactory.getInstance("X.509");   // jedyny dosteny typ into: https://docs.oracle.com/javase/8/docs/technotes/guides/security/StandardNames.html
+            InputStream certInput = new BufferedInputStream(new FileInputStream(Constants.CERTIFICATE_PATH));
+            Certificate cert = factory.generateCertificate(certInput);
+
+            String keyStoreType = KeyStore.getDefaultType();
+            KeyStore keyStore = KeyStore.getInstance(keyStoreType);
+            keyStore.load(null, null);
+            keyStore.setCertificateEntry("ca", cert);
+
+            String trustManagerAlg = TrustManagerFactory.getDefaultAlgorithm();
+            TrustManagerFactory trustFactory = TrustManagerFactory.getInstance(trustManagerAlg);
+            trustFactory.init(keyStore);
+
+            SSLContext sslContext = SSLContext.getInstance("TLS");
+            sslContext.init(null, trustFactory.getTrustManagers(), null);
+
+            TrustManager[] trustManagers = trustFactory.getTrustManagers();
+            if (trustManagers.length == 1 && (trustManagers[0] instanceof X509TrustManager))
+                return trustManagers[0];
+
+        } catch (CertificateException | NoSuchAlgorithmException | KeyManagementException | KeyStoreException | IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
     }
 }
