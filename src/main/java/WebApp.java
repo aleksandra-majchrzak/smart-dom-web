dcb2802c17676f7d33956dbb708bbe5b5e3bf166
aleksandra-majchrzak <aleksandra.a.majchrzak@gmail.com>
2017-08-20 23:09:53 +0200

    Added initial mDNS config.

--- src/main/java/WebApp.java
+++ src/main/java/WebApp.java
@@ -4,6 +4,16 @@ import controllers.UserController;
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 
+import javax.jmdns.JmDNS;
+import javax.jmdns.ServiceEvent;
+import javax.jmdns.ServiceInfo;
+import javax.jmdns.ServiceListener;
+import java.io.IOException;
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -78,9 +88,73 @@ public class WebApp {
 
         get("/modules/:id", (request, response) ->
                 render(ModulesController.getModule(request, response)));
+
+        startJmDNS();
     }
 
     private static String render(ModelAndView modelAndView) {
         return new VelocityTemplateEngine().render(modelAndView);
     }
+
+    private static void startJmDNS() {
+        try {
+            // Create a JmDNS instance
+            //JmDNS jmdns = JmDNS.create(InetAddress.getLocalHost());
+            JmDNS jmdns = JmDNS.create(getIpAddress());
+
+            // Register a service
+            ServiceInfo serviceInfo = ServiceInfo.create("_http._tcp.local.", "smart_dom", 4567, "path=index.html");
+            jmdns.registerService(serviceInfo);
+
+            // Wait a bit
+            //Thread.sleep(25000);
+
+            jmdns.addServiceListener("_http._tcp.local.", new SampleListener());
+
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private static InetAddress getIpAddress() {
+        try {
+            InetAddress result = null;
+            for (Enumeration en = NetworkInterface.getNetworkInterfaces(); en.hasMoreElements(); ) {
+                NetworkInterface intf = (NetworkInterface) en.nextElement();
+                for (Enumeration enumIpAddr = intf.getInetAddresses(); enumIpAddr.hasMoreElements(); ) {
+                    InetAddress inetAddress = (InetAddress) enumIpAddr.nextElement();
+                    if (!inetAddress.isLoopbackAddress() && inetAddress instanceof Inet4Address
+                            && intf.getName().contains("wlan")) {
+                        String ipAddress = inetAddress.getHostAddress();
+                        System.out.println("IP address: " + ipAddress);
+                        //return inetAddress;
+                        result = inetAddress;
+                    }
+                }
+            }
+            return result;
+        } catch (SocketException ex) {
+            System.out.println("Socket exception in GetIP Address of Utilities: " + ex.toString());
+        }
+        return null;
+    }
+
+
+    private static class SampleListener implements ServiceListener {
+        @Override
+        public void serviceAdded(ServiceEvent event) {
+            System.out.println("Service added: " + event.getInfo());
+        }
+
+        @Override
+        public void serviceRemoved(ServiceEvent event) {
+            System.out.println("Service removed: " + event.getInfo());
+        }
+
+        @Override
+        public void serviceResolved(ServiceEvent event) {
+            System.out.println("Service resolved: " + event.getInfo());
+        }
+    }
+
 }
