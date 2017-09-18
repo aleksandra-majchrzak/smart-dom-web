48763d70fbd6264439a8ea4ff6357cc02b079fc2
aleksandra-majchrzak <aleksandra.a.majchrzak@gmail.com>
2017-09-18 17:56:48 +0200

    Changed keystore.

--- src/main/java/pl/uj/edu/ii/smartdom/web/Constants.java
+++ src/main/java/pl/uj/edu/ii/smartdom/web/Constants.java
@@ -4,8 +4,8 @@ package pl.uj.edu.ii.smartdom.web;
  * Created by Mohru on 10.08.2017.
  */
 public interface Constants {
-    String KEYSTORE_PATH = "smart_keystore.jks";
-    String KEYSTORE_PASS = "Smart_password1";
+    String KEYSTORE_PATH = "smart_dom_keystore.jks";
+    String KEYSTORE_PASS = "Smart_password3";
     String CERTIFICATE_PATH = "X509_certificate.cer";
     String SMART_DOM = "SmartDom";
 
